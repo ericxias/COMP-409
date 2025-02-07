a3a1df10db9d99d88f86e3340e21b23ace6bce25
ericxias <90631685+ericxias@users.noreply.github.com>
2025-02-06 19:05:49 -0500

    7 moves preselected implemented

--- a2/q1.java
+++ a2/q1.java
@@ -40,19 +40,6 @@ public class q1 {
             e.printStackTrace();
         }
 
-        // Read and add words to the dictionary from dictionary.txt
-        File file2 = new File("dictionary.txt");
-        try (Scanner reader = new Scanner(file2)){
-            while (reader.hasNextLine()){
-                String data = reader.nextLine().trim();
-                if (!data.isEmpty()){
-                    dictionary.add(data);
-                };
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
         // Create and draw the grid
         grid = new Cell[n][n];
         Random random = new Random(s);
@@ -83,6 +70,19 @@ public class q1 {
             System.out.println();
         }
 
+        // Read and add words to the dictionary from dictionary.txt
+        File file2 = new File("dictionary.txt");
+        try (Scanner reader = new Scanner(file2)){
+            while (reader.hasNextLine()){
+                String data = reader.nextLine().trim();
+                if (!data.isEmpty()){
+                    dictionary.add(data);
+                };
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         /*
          * launch t threads to conduct a random search for words. Each thread picks a random starting cell,
          * and pre-selects a sequence of up to 7 random moves by incrementally creating a plan of moves to make.
@@ -97,7 +97,25 @@ public class q1 {
                 @Override
                 public void run(){
                     Random random = new Random();
-                    
+                    // each thread tests k starting cells
+                    for (int i = 0; i < k; i++) {
+                        // random starting cell
+                        int x = random.nextInt(n);
+                        int y = random.nextInt(n);
+                        // list to store moves
+                        List<int[]> moves = new ArrayList<>();
+                        moves.add(new int[]{x, y});
+
+                        // pre-select a sequence of up to 7 random moves
+                        for (int j = 0; j < 6; j++){
+                            int[] current = moves.get(moves.size() - 1);
+                            int currx = current[0];
+                            int curry = current[1];
+                            int[] nextMove = getNextMove(currx, curry, n, moves);
+                            moves.add(nextMove);
+                        }
+                        
+                    }
                 }
             });
             threads[i].start();
@@ -115,11 +133,25 @@ public class q1 {
         
     }
 
+    public static int[] getNextMove(int x, int y, int n, List<int[]> moves){
+        // get all possible next moves, check if they are valid, and return a random next move
+        Random random = new Random();
+        List<int[]> possibleMoves = new ArrayList<>();
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+        for (int[] direction: directions){
+            int nextx = x + direction[0];
+            int nexty = y + direction[1];
+            if (nextx >= 0 && nextx < n && nexty >= 0 && nexty < n && !moves.contains(new int[]{nextx, nexty})){
+                possibleMoves.add(new int[]{nextx, nexty});
+            }
+        }
+        return possibleMoves.get(random.nextInt(possibleMoves.size()));
+    }
 
     // Cell class with locks
     public static class Cell {
         private final char letter;
-        // possible words to be formed from this cell
+        // words the cell contributed to
         private final List<String> words;
         private final ReentrantLock lock;
 
@@ -139,6 +171,10 @@ public class q1 {
 
         public ReentrantLock getLock(){
             return this.lock;
+        }
+
+        public void addWord(String word){
+            this.words.add(word);
         }
     }
 }
